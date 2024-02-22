0590c6dbf53c1aab889332a31fbda060d21c48e9
CowChicken <Eric.zwirnmann@gmail.com>
2024-02-22 18:57:21 -0500

    Added title overlay and nav to Lesson2

---
+++ Assets/Scripts/Lesson1Manager.cs
@@ -0,0 +1,24 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Lesson1Manager : MonoBehaviour
+{
+    public TMP_Text RaceTeamText;
+
+    // Start is called once before any Update methods are called
+    void Start()
+    {
+        RaceTeamText.text = PlayerSettings.Instance.RaceTeamName;    
+    }
+
+    public void OnLesson2Click()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void OnMenuClick()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
